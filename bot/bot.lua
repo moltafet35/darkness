59c7714cb92a77207649d5b3b8b77ffcffc2957d
moltafet35 <moltafet@mailfa.com>
2015-12-12 14:11:20 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {76534150},
+    sudo_users = {69149583},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
